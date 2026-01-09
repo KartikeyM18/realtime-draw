f9f72e20f348cab5683d0e89fe0425c4a0412c02
Kartikey <kadm5224@gmail.com>
2026-01-09 17:24:36 +0530

    Added JWT to ws

--- backend/ws/src/index.ts
+++ backend/ws/src/index.ts
@@ -1,10 +1,26 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
+import type { AuthPayload } from "./types/authPayload.js";
 
 const port = 4001;
 const wss = new WebSocketServer({port: port});
 
 
-wss.on("connection", (ws)=>{
+wss.on("connection", (ws, request)=>{
+    const url = request.url;
+    if(!url) return;
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') ?? "";
+
+    const decoded = jwt.verify(token, JWT_SECRET) as AuthPayload;
+
+    if(!decoded || !decoded.userId){
+        ws.close();
+        return;
+    }
+
     ws.on("error", console.error);
     
     ws.on("message", (data)=>{
