c776f453fc2b5462d145b42ba1102dd85d19b0fb
Kartikey <kadm5224@gmail.com>
2026-01-12 17:09:08 +0530

    Implemented schema in http

--- backend/http/src/index.ts
+++ backend/http/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "./config.js";
 import { verifyJwt } from "./middlewares/middlewares.js";
+import { RoomSchema, SigninSchema, SignupSchema } from "@kadm/draw-common";
 
 const app = express();
 
@@ -13,16 +14,32 @@ app.get("/", (req, res) => {
 })
 
 app.post("/signup", (req, res) => {
+    const data = SignupSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({msg: "Incorrect inputs"});
+    }
+
     res.json({userId: "1"});
 })
 
 app.get("/signin", (req, res) => {
+
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({msg: "Incorrect inputs"});
+    }
+
     const userId = req.userId;
     const token = jwt.sign({userId}, JWT_SECRET);
     res.json({token});
 })
 
 app.post("/room", verifyJwt, (req, res) => {
+    const data = RoomSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({msg: "Incorrect inputs"});
+    }
+
     res.json({roomId: 1});
 })
 
