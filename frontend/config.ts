56d73c6b3a78613a2a593097207a9a871760be9d
Kartikey <kadm5224@gmail.com>
2026-04-07 23:45:36 +0530

    Ws backend url config fixed

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,3 @@
 export const HTTP_BACKEND = process.env.NEXT_PUBLIC_HTTP as string;
-// export const WS_BACKEND = process.env.NEXT_PUBLIC_WS as string;
-export const WS_BACKEND = "ws://localhost:4001";
+export const WS_BACKEND = process.env.NEXT_PUBLIC_WS as string;
+// export const WS_BACKEND = "ws://localhost:4001";
